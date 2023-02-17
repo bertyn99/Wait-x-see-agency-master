7acf7122d8e83ad4929aa42872fd2a2db89067af
Bertyn Boulikou <bboulikou@gmail.com>
2023-02-17 18:36:24 +0100

    maj file to rails 6

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,10 +6,9 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules public')
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w(owl.carousel.min.js owl.carousel.min.css scrollreveal.js noscrollbar.js index.js index.css accueil2.css header.css footer.css managing.css marketing.css branding.css contact.css css/plugins/plugins.css)
