77fcb086eadf52e161d3e1457916a72cddf97b43
Bertyn Boulikou <bboulikou@gmail.com>
2023-03-06 21:41:43 +0100

    add index.css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,7 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( index.js noscrollbar.js owl.carousel.min.js )
-Rails.application.config.assets.precompile += %w( header.css accueil2.css contact.css branding.css marketing.css  footer.css managing.css)
+Rails.application.config.assets.precompile += %w( index.css header.css accueil2.css contact.css branding.css marketing.css  footer.css managing.css)
 Rails.application.config.assets.precompile += %w( owl.carousel.min.css css/plugins/plugins.css )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
